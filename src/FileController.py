9f06c6ea4a4305533244dbd2e853744ebf5cc8a5
we-yu <whiterythronium@gmail.com>
2019-04-16 04:04:34 +0700

    Update Scraper

---
+++ src/FileController.py
@@ -0,0 +1,8 @@
+# coding: UTF-8
+# View
+
+class FileCtrl :
+
+    def __init__(self) :
+        print('Call ' + self.__class__.__name__ + ' Constructor')
+
--- src/GUICtrl.py
+++ src/GUICtrl.py
@@ -1,27 +1,38 @@
 # coding: UTF-8
-
+# View
 import tkinter as tk
 import os
 import PIL.ImageTk as pilimgtk
 import PIL.Image as pilimg
 from pprint import pprint
 import IconScraper
-
+import FileController
 
 class GUIController :
 
 #   Class value ------------------------------------
+    TARGET_URL = 'https://store.line.me/stickershop/product/4333/ja'
+
     MAXIMUM_COLUMN = 4
     __windowWidth   = 0
     __windowHeight  = 0
 #   Setting property -------------------------------
+    # tkinter root
     @property
     def root(self) :
         return self.__root
     @root.setter
     def root(self, value) :
         self.__root = value
-#   ------------------------------------------------
+
+    # Load target Url
+    @property
+    def tgtStiUrl(self):
+        return self.__tgtStiUrl
+    @tgtStiUrl.setter
+    def tgtStiUrl(self, value):
+        self.__tgtStiUrl = value
+    #   ------------------------------------------------
 
     def __init__(self, title, width, height) :
         GUIController.__windowWidth   = width
@@ -36,11 +47,15 @@ class GUIController :
         geometryText = str(GUIController.__windowWidth) + 'x' + str(GUIController.__windowHeight)
         self.root.geometry(geometryText)
 
-        # Generate window move to front
+        # Generate window move to front [Warning]
         self.root.attributes("-topmost", True)
 
         # Set Focus on generated window
         os.system("open -a Python")
+
+        # Temp --------------------
+        self.SetTargetStickerUrl(GUIController.TARGET_URL)
+        # Temp --------------------
 
         return
 
@@ -49,14 +64,21 @@ class GUIController :
         cv_width    = 200
         cv_height   = 200
 
+        # Scaper instantiate
+        iconScraper = IconScraper.IconScraper(self.tgtStiUrl)
         # Load all image files
-        self.tkimgs = self.GetPhotoImages()
+        self.tkimgs = self.GetPhotoImages(iconScraper)
 
         bg_RGB = [0, 0, 0]
 
         gridRow = 0
         gridCol = 0
         for i, tkimg in enumerate(self.tkimgs) :
+
+            # 1 2 3 4
+            # 5 6 □ □
+            # □ □ □ □
+            # □ □ □ □
 
             bg_RGB[0] = (i * 20)
             bg_RGB[1] = (i * 20)
@@ -72,6 +94,7 @@ class GUIController :
             self.cv.grid(row=gridRow, column=gridCol)
             # self.cv.pack(anchor=tk.NW, side=tk.LEFT)
 
+            # 画像はCanvasの真ん中に配置
             self.cv.create_image((cv_width / 2), (cv_height / 2), image=tkimg)
 
             gridCol += 1
@@ -80,28 +103,10 @@ class GUIController :
                 gridRow += 1
                 gridCol  = 0
 
-            # 1 2 3 4
-            # 5 6 □ □
-            # □ □ □ □
-            # □ □ □ □
+    def SetTargetStickerUrl(self, url) :
+        self.tgtStiUrl = url
 
-# for i in range(4) :
-        #     bg_RGB[0] = (i * 20)
-        #     bg_RGB[1] = (i * 20)
-        #     bg_RGB[2] = (i * 20)
-        #     rgbText = '#' + ''.join(map(str, bg_RGB))
-        #
-        #     # No canvas padding
-        #     self.cv = tk.Canvas(self.root, width=cv_width, height=cv_height, bg=rgbText, highlightthickness=0, relief='ridge')
-        #     self.cv.place(x=0, y=0)
-        #     # self.cv.create_rectangle(5, 5, 20, 20, fill = 'green', outline = 'red')
-        #
-        #     # CanvasはWindowの左上から敷き詰める
-        #     self.cv.pack(anchor=tk.N, side=tk.LEFT)
-        #     # 画像はCanvasの真ん中に配置
-        #     self.cv.create_image((cv_width / 2), (cv_height / 2), image=self.tkimgs[i])
-
-    def GetPhotoImages(self) :
+    def GetPhotoImages(self, scraper) :
         imgs = []
         photoImgs = []
         imgs.append(r'/Users/yuji/Documents/python/L_SL/img/icons/pte1.png')
@@ -110,14 +115,29 @@ class GUIController :
         imgs.append(r'/Users/yuji/Documents/python/L_SL/img/icons/pte4.png')
         imgs.append(r'/Users/yuji/Documents/python/L_SL/img/icons/pte5.png')
 
-        iconScraper = IconScraper.IconScraper()
-        iconScraper.GetAllIconURL()
+        urls = scraper.GetAllIconURL()
+        pprint(urls)
+
+        title = scraper.GetStickerTitle()
+        self.IconSave(title, self.tgtStiUrl)
 
         for img in imgs :
+            # PNG convert to jpeg [Warning]
             openImg = pilimg.open(img).convert('RGB')
             photoImgs.append(pilimgtk.PhotoImage(openImg))
 
         return photoImgs
+
+    def IconSave(self, title, url):
+
+        urlId = url.split('/')[5]
+        dirName = title.replace(' ', '_')
+        dirName = dirName.replace('/', '／')
+        dirName = urlId + '_' + dirName
+
+        print(dirName)
+
+        return True
 
     def ShowWindow(self):
         self.root.mainloop()
